75353115aea544081f46dea247eb211b93d1d455
georgezhao2010 <involute@sina.com>
2020-11-10 08:59:10 +0800

    first commit

---
+++ custom_components/peacefair_energy_monitor/modbus.py
@@ -0,0 +1,146 @@
+import logging
+
+from pymodbus.client.sync import ModbusTcpClient, ModbusUdpClient, ModbusSerialClient
+from pymodbus.transaction import ModbusRtuFramer, ModbusIOException
+from pymodbus.pdu import ModbusRequest
+import threading
+import time
+
+from homeassistant.const import (
+    DEVICE_CLASS_VOLTAGE ,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_POWER_FACTOR
+)
+
+from .const import(
+    DEVICE_CLASS_FREQUENCY
+)
+
+HPG_SENSOR_TYPES = [
+    DEVICE_CLASS_VOLTAGE ,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_ENERGY ,
+    DEVICE_CLASS_POWER_FACTOR,
+    DEVICE_CLASS_FREQUENCY
+]
+
+_LOGGER = logging.getLogger(__name__)
+
+class ModbusResetEnergyRequest(ModbusRequest):
+    _rtu_frame_size = 4
+    function_code = 0x42
+    def __init__(self, **kwargs):
+        ModbusRequest.__init__(self, **kwargs)
+
+    def encode(self):
+        return b''
+
+    def get_response_pdu_size(self):
+        return 4
+
+    def __str__(self):
+        return "ModbusResetEnergyRequest"
+
+class ModbusHub:
+    def __init__(self, protocol, host, port):
+        self._lock = threading.Lock()
+        if(protocol == "rtuovertcp"):
+            self._client = ModbusTcpClient(
+                host = host,
+                port = port,
+                framer = ModbusRtuFramer,
+                timeout = 2,
+                retry_on_empty = True,
+            )
+        elif (protocol == "rtuoverudp"):
+            self._client = ModbusUdpClient(
+                host = host,
+                port = port,
+                framer = ModbusRtuFramer,
+                timeout = 2,
+                retry_on_empty = True,
+            )
+            
+    def connect(self):
+        with self._lock:
+            self._client.connect()
+
+    def close(self):
+        with self._lock:
+            self._client.close()
+
+    def read_holding_register(self):
+        pass
+
+    def read_input_registers(self, slave, address, count):
+        with self._lock:
+            kwargs = {"unit": slave} if self else {}
+            return self._client.read_input_registers(address, count, **kwargs)
+
+    def reset_energy(self, slave):
+        with self._lock:
+            kwargs = {"unit": slave} if self else {}
+            request = ModbusResetEnergyRequest(**kwargs)
+            self._client.execute(request)
+
+class ModbusGather(ModbusHub, threading.Thread):
+    def __init__(self, hass, slave, protocol, host, port, scan_interval):
+        ModbusHub.__init__(self, protocol, host, port)
+        threading.Thread.__init__(self)
+        self._entity_id_base = "{}_{}_{}".format(
+            host, port, slave
+        )
+        self._hass = hass
+        self._entity_id_base = self._entity_id_base.replace(".", "_")
+        self._slave = slave
+        self._run = False
+        self._interval = scan_interval
+
+    def infogather(self):
+
+        result = self.read_input_registers(self._slave, 0, 9)
+        if result is not None and type(result) is not ModbusIOException \
+                and result.registers is not None and len(result.registers) == 9:
+            data = {}
+
+            data[DEVICE_CLASS_VOLTAGE] = result.registers[0] / 10
+            data[DEVICE_CLASS_CURRENT] = ((result.registers[2] << 16) + result.registers[1]) / 1000
+            data[DEVICE_CLASS_POWER] = ((result.registers[4] << 16) + result.registers[3]) / 10
+            data[DEVICE_CLASS_ENERGY] = (result.registers[6] << 16) + result.registers[5] / 1000
+            data[DEVICE_CLASS_FREQUENCY] = result.registers[7] / 10
+            data[DEVICE_CLASS_POWER_FACTOR] = result.registers[8] / 100
+            for sensor_type in HPG_SENSOR_TYPES:
+                entity_id ="sensor.{}_{}".format(self._entity_id_base, sensor_type)
+                attrs = self._hass.states.get(entity_id).attributes
+                self._hass.states.async_set(entity_id, data[sensor_type], attrs)
+
+    def run(self):
+        self.connect()
+        counter = 0
+        while self._run:
+            counter = counter + 1
+            if counter >= self._interval:
+                self.infogather()
+                counter = 0
+            time.sleep(1)
+
+        self._client.close()
+
+    def start_keep_alive(self):
+        self._run = True
+        threading.Thread.start(self)
+
+    def stop_gather(self):
+        self._run = False
+        threading.Thread.stop(self)
+        threading.Thread.join(self)
+
+    async def async_reset_energy(self):
+        self.reset_energy(self._slave)
+
+    def set_interval(self, interval):
+        self._interval = interval
+
---
+++ custom_components/peacefair_energy_monitor/sensor.py
@@ -0,0 +1,112 @@
+from homeassistant.const import (
+    DEVICE_CLASS_VOLTAGE ,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_ENERGY ,
+    DEVICE_CLASS_POWER_FACTOR,
+    VOLT,
+    ELECTRICAL_CURRENT_AMPERE,
+    POWER_WATT,
+    ENERGY_KILO_WATT_HOUR,
+    FREQUENCY_HERTZ
+)
+
+from .const import(
+    DOMAIN,
+    MODBUS_HUB,
+    IDENT,
+    ENERGY_SENSOR,
+    DEVICE_CLASS_FREQUENCY
+)
+
+from homeassistant.helpers.entity import Entity
+
+HPG_SENSOR_TYPES = [
+    DEVICE_CLASS_VOLTAGE ,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_ENERGY ,
+    DEVICE_CLASS_POWER_FACTOR,
+    DEVICE_CLASS_FREQUENCY
+]
+
+NAMES = {
+    DEVICE_CLASS_VOLTAGE: "Peacefair Voltage",
+    DEVICE_CLASS_CURRENT: "Peacefair Current",
+    DEVICE_CLASS_POWER: "Peacefair Power",
+    DEVICE_CLASS_ENERGY: "Peacefair Energy",
+    DEVICE_CLASS_POWER_FACTOR: "Peacefair Power Factor",
+    DEVICE_CLASS_FREQUENCY: "Peacefair Power Frequency"
+}
+
+UNITS = {
+    DEVICE_CLASS_VOLTAGE: VOLT,
+    DEVICE_CLASS_CURRENT: ELECTRICAL_CURRENT_AMPERE,
+    DEVICE_CLASS_POWER: POWER_WATT,
+    DEVICE_CLASS_ENERGY: ENERGY_KILO_WATT_HOUR,
+    DEVICE_CLASS_FREQUENCY: FREQUENCY_HERTZ
+}
+
+ICONS = {
+    DEVICE_CLASS_FREQUENCY: "hass:current-ac"
+}
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    sensors = []
+    ident = hass.data[config_entry.entry_id][IDENT]
+    for sensortype in HPG_SENSOR_TYPES:
+        sensor = HPGSensor(config_entry.entry_id, sensortype, ident)
+        sensors.append(sensor)
+        if sensor.device_class == sensortype:
+            if ENERGY_SENSOR not in hass.data[DOMAIN]:
+                hass.data[DOMAIN][ENERGY_SENSOR] = []
+            hass.data[DOMAIN][ENERGY_SENSOR].append(sensor)
+    async_add_entities(sensors)
+
+class HPGSensor(Entity):
+    def __init__(self, entry_id, sensor_type, ident):
+        self._unique_id = "sensor.{}_{}".format(ident, sensor_type)
+        self._entry_id = entry_id
+        self.entity_id = self._unique_id
+        self._sensor_type = sensor_type
+        self._device_info = {
+            "identifiers": {(DOMAIN, ident)},
+            "manufacturer": "Peacefair",
+            "model": "PZEM-004T",
+            "name": "Peacefair Power Gather"
+        }
+    def get_entry_id(self):
+        return self._entry_id
+
+    @property
+    def assumed_state(self):
+        return False
+
+    @property
+    def should_poll(self):
+        return False
+
+    @property
+    def device_info(self):
+        return self._device_info
+
+    @property
+    def device_class(self):
+        return self._sensor_type
+
+    @property
+    def name(self):
+        return NAMES.get(self._sensor_type)
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+    @property
+    def unit_of_measurement(self):
+        return UNITS.get(self._sensor_type)
+
+    @property
+    def icon(self):
+        return ICONS.get(self._sensor_type)
+
