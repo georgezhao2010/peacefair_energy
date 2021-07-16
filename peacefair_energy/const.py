46d2405a1ed18307ef5563af18502b47d6b6e1eb
George Zhao <involute@sina.com>
2021-07-16 19:49:58 +0800

    rename to peacefair_energy

---
+++ peacefair_energy/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "peacefair_energy_monitor"
+DEFAULT_SCAN_INTERVAL = 10
+DEFAULT_SLAVE = 1
+DEFAULT_PROTOCOL = "rtuoverudp"
+DEFAULT_PORT = 9000
+MODBUS_HUB = "modbus_hub"
+IDENT = "identification"
+ENERGY_SENSOR = "energy_sensor"
+UN_SUBDISCRIPT = "un_subdiscript"
+DEVICE_CLASS_FREQUENCY = "frequency"
+DEVICES = "devices"
+VERSION = "0.2.11"
